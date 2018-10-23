38dd89976f1596ecbdb668d246bb9f9641d9802a
xue.zeng <xue.zeng@outlook.com>
2018-10-23 10:23:34 +0800

    demo(Java):添加restdocs测试

--- demo/Java/encrypt/encrypt-rest/src/main/java/me/xue/encrypt/rest/controller/Base64Controller.java
+++ demo/Java/encrypt/encrypt-rest/src/main/java/me/xue/encrypt/rest/controller/Base64Controller.java
@@ -1,26 +1,45 @@
 package me.xue.encrypt.rest.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Slf4j
-@Api(value = "/Base64Controller", description = "Base64加解密JDK8实现")
+@Api(value = "Base64Controller", description = "Base64加解密JDK8实现")
 @RestController
+@RequestMapping("/base64")
 public class Base64Controller {
 
     @ApiOperation(value = "获取Base64加密字符", notes = "JDK自带实现Base64加密算法")
     @ApiImplicitParam(name = "text", value = "加密文本", dataType = "String", paramType = "query")
-    @RequestMapping("/base64OfJdk")
-    public String base64OfJdk(@RequestParam(required = true) String text) {
-        String base64 = Base64.getEncoder().encodeToString(text.getBytes());
-        log.info(" The {} JDK base64 of {}", text, base64);
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "加密成功", response = String.class),
+            @ApiResponse(code = 500, message = "加密失败", response = String.class)
+    })
+    @RequestMapping(value = "/jdk/encoder")
+    public String base64EncoderOfJdk(@RequestParam(required = true) String text) {
+        String base64 = Base64.getEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        log.info(" The {} JDK base64 encoder of {}", text, base64);
         return base64;
     }
+
+    @RequestMapping(value = "/jdk/urlencoder")
+    public String base64UrlEncoderOfJdk(@RequestParam(required = true) String text) {
+        String base64UrlStr = Base64.getUrlEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        log.info(" The {} JDK base64 url encoder of {}", text, base64UrlStr);
+        return base64UrlStr;
+    }
+
+    @RequestMapping(value = "/jdk/mimeencoder")
+    public String base64MimeEncoderOfJdk(@RequestParam(required = true) String text) {
+        String base64UrlStr = Base64.getMimeEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+        log.info(" The {} JDK base64 url encoder of {}", text, base64UrlStr);
+        return base64UrlStr;
+    }
+
 }
--- demo/Java/encrypt/encrypt-rest/src/test/java/me/xue/encrypt/rest/controller/Base64ControllerTests.java
+++ demo/Java/encrypt/encrypt-rest/src/test/java/me/xue/encrypt/rest/controller/Base64ControllerTests.java
@@ -1,20 +1,25 @@
-package me.xue.encrypt.rest;
+package me.xue.encrypt.rest.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ExtendWith({ RestDocumentationExtension.class, SpringExtension.class})
-public class EncryptRestApplicationTests {
+public class Base64ControllerTests {
 
     private MockMvc mockMvc;
 
@@ -25,6 +30,16 @@ public class EncryptRestApplicationTests {
                 .webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
+    }
+
+    @Test
+    public void base64OfJdk() throws Exception {
+        this.mockMvc.perform(
+                RestDocumentationRequestBuilders
+                        .get("/base64/jdk").param("text", "123456"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("base64/jdk"));
     }
 
 }
