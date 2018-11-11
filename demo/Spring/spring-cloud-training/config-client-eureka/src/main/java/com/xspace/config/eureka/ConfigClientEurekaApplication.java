585a1ff28ee61a87adedbbad0ad46d387374fa78
xue.zeng <xue.zeng@outlook.com>
2018-11-11 18:06:04 +0800

    demo(spring):spring cloud学习示例
    1.config-server-eureka
    2.config-client-eureka

---
+++ demo/Spring/spring-cloud-training/config-client-eureka/src/main/java/com/xspace/config/eureka/ConfigClientEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.xspace.config.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientEurekaApplication.class, args);
+    }
+}
