6432fba376b1955fd416c9a803f4cdde5a9560cc
xue.zeng <xue.zeng@accenture.com>
2019-04-11 16:06:13 +0800

    demo(springboot):添加mybatis示例

---
+++ demo/Spring/spring-boot-training/spring-boot-mybatis/src/test/java/com/xspace/mybatis/SpringBootMybatisApplicationTests.java
@@ -0,0 +1,11 @@
+package com.xspace.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMybatisApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
