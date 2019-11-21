a0954bb721dccf2e015c9d55a002c1efb00e72ac
xue.zeng <xue.zeng@accenture.com>
2019-11-21 10:50:17 +0800

    update note

--- demo/Go/src/gopl/ch01/exercises/1.7/main.go
+++ demo/Go/src/gopl/ch01/exercises/1.7/main.go
@@ -2,6 +2,7 @@
 // Copyright © 2015 Alan A. A. Donovan · Brian W. Kernighan.
 // Published Oct 26, 2015 in paperback and Nov 20 in e-book
 
+// 1.7 函数调用io.Copy(dst, src)会从src中读取内容，并将读到的结果写入到dst中，使用 这个函数替代掉例子中的ioutil.ReadAll来拷贝响应结构体到os.Stdout，避免申请一个缓冲区 （例子中的b）来存储。记得处理io.Copy返回结果中的错误。
 package main
 
 import (
